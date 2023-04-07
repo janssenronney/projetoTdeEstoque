2388827be8ef3c7b411d283cfa5400150f515622
janssenronney <janssenronney@gmail.com>
2023-04-06 22:43:10 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const telaLogin = document.querySelector('.tela-login');
+//const entrarLink = document.querySelector('.entrar-link');
+//const registrarLink = document.querySelector('.registrar-link');
+const botaoEntrar = document.querySelector('.botao');
+
+//registrarLink.addEventListener("click", ()=> {
+    //console.log(telaLogin);
+//    telaLogin.classList.add("active");
+//});
+
+//entrarLink.addEventListener("click", ()=> {    
+//    telaLogin.classList.remove("active");
+//});
+
+botaoEntrar.addEventListener('click', ()=> {
+    telaLogin.classList.add('active-popup');
+});
